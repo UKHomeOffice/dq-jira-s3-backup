69f85ddb85dfef12f2d7b8b44086b84d43694bfc
Duncan Mountford <duncan9562@gmail.com>
2018-09-10 13:38:57 +0100

    Removed #'s in s3-backup.sh and renamed log file in start.sh

--- start.sh
+++ start.sh
@@ -7,13 +7,12 @@ CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
 echo "access_key=$ACCESS_KEY" >> /root/.s3cfg
 echo "secret_key=$SECRET_KEY" >> /root/.s3cfg
 
-LOGFIFO='/var/log/cron.fifo'
-if [[ ! -e "$LOGFIFO" ]]; then
-    mkfifo "$LOGFIFO"
+LOGFILE='/var/log/cron.log'
+if [[ ! -e "$LOGFILE" ]]; then
+    mkfifo "$LOGFILE"
 fi
 
-CRON_ENV="PARAMS='$PARAMS'"
-echo -e "$CRON_ENV\n$CRON_SCHEDULE /s3-backup.sh > $LOGFIFO 2>&1" | crontab -
+echo -e "$CRON_SCHEDULE /s3-backup.sh > $LOGFILE 2>&1" | crontab -
 crontab -l
 cron
-tail -f "$LOGFIFO"
+tail -f "$LOGFILE"
