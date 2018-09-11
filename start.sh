d3e243463960c93aac9941f81cd23a4d7274eb70
Duncan Mountford <duncan9562@gmail.com>
2018-09-11 10:20:49 +0100

    Remove old references to /root/.s3cfg in start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,6 @@ set -e
 
 CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
 
-echo "access_key=$ACCESS_KEY" >> /root/.s3cfg
-echo "secret_key=$SECRET_KEY" >> /root/.s3cfg
-
 LOGFILE='/var/log/cron.log'
 if [[ ! -e "$LOGFILE" ]]; then
     mkfifo "$LOGFILE"
