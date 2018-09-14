95362f13534117948afb1c37fc18749786b2590b
Duncan Mountford <duncan9562@gmail.com>
2018-09-14 11:08:12 +0100

    Change arguement numbers

--- s3-backup.config.js
+++ s3-backup.config.js
@@ -7,14 +7,14 @@ module.exports = {
     	env: {
     	  START_HOUR_1: 06,
     	  START_HOUR_2: 18,
-    	  DATABASE_HOST: process.argv[5],
-    	  DATABASE_NAME: process.argv[6],
-    	  PGPASSWORD: process.argv[7],
-    	  DATABASE_PORT: process.argv[8],
-    	  DATABASE_USERNAME: process.argv[9],
-    	  BUCKET_NAME: process.argv[10],
-    	  AWS_ACCESS_KEY_ID: process.argv[11],
-    	  AWS_SECRET_ACCESS_KEY: process.argv[12]
+    	  DATABASE_HOST: process.argv[6],
+    	  DATABASE_NAME: process.argv[7],
+    	  PGPASSWORD: process.argv[8],
+    	  DATABASE_PORT: process.argv[9],
+    	  DATABASE_USERNAME: process.argv[10],
+    	  BUCKET_NAME: process.argv[11],
+    	  AWS_ACCESS_KEY_ID: process.argv[12],
+    	  AWS_SECRET_ACCESS_KEY: process.argv[13]
     	}
     }
   ]
